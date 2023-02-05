c928ee2e84690b272a2f7ba829e406a81fcb4496
victluna <107931486+victluna@users.noreply.github.com>
2023-02-05 10:38:52 +0800

    Update earlyStop.py

--- earlyStop.py
+++ earlyStop.py
@@ -1,21 +1,11 @@
-
 import os
-
 import torch
-
-
 class EarlyStopping():
     """
     Early stopping to stop the training when the loss does not improve after
     certain epochs.
     """
     def __init__(self, patience=5, min_delta=0):
-        """
-        :param patience: how many epochs to wait before stopping when loss is
-               not improving
-        :param min_delta: minimum difference between new loss and old loss for
-               new loss to be considered as an improvement
-        """
         self.patience = patience
         self.min_delta = min_delta
         self.counter = 0
@@ -26,8 +16,6 @@ class EarlyStopping():
             self.best_loss = loss
         elif self.best_loss - loss > self.min_delta:
             self.best_loss = loss
-            # torch.save(model.module.state_dict(), os.path.join(args.output_dir, 'param_' + name + '_WQ_' + str(iteration) +'.pt'), _use_new_zipfile_serialization = False)
-            # reset counter if validation loss improves
             self.counter = 0
         elif self.best_loss - loss < self.min_delta:
             self.counter += 1
@@ -38,7 +26,5 @@ class EarlyStopping():
 
 if __name__ == '__main__':
     early = EarlyStopping()
-    early(5)
-    early(6)
-    print(early.early_stop)
+    
    
