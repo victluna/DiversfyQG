a017d4f9156357001186ec9d3aa49243a409471b
victluna <107931486+victluna@users.noreply.github.com>
2023-02-05 11:17:29 +0800

    Update data.py

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ from utils.misc import invert_dict
 
 def collate(batch):
     batch = list(zip(*batch))
-    source_ids = torch.stack(batch[0])#stack与cat均为拼接函数，只是stack在拼接后会多一维
+    source_ids = torch.stack(batch[0])
     source_mask = torch.stack(batch[1])
     if batch[-1][0] is None:
         target_ids = None
@@ -18,21 +18,17 @@ class Dataset(torch.utils.data.Dataset):
         self.source_ids, self.source_mask, self.target_ids = inputs
         self.is_test = len(self.target_ids)==0
 
-
     def __getitem__(self, index):
         source_ids = torch.LongTensor(self.source_ids[index])
         source_mask = torch.LongTensor(self.source_mask[index])
        
         if self.is_test:
-            target_ids = None
-           
+            target_ids = None           
         else:
-            target_ids = torch.LongTensor(self.target_ids[index])
-          
+            target_ids = torch.LongTensor(self.target_ids[index])          
         return source_ids, source_mask, target_ids
     def __len__(self):
         return len(self.source_ids)
-
 
 class DataLoader(torch.utils.data.DataLoader):
     def __init__(self, question_pt, batch_size, training=False):
@@ -43,20 +39,12 @@ class DataLoader(torch.utils.data.DataLoader):
                 inputs.append(pickle.load(f))
         dataset = Dataset(inputs)
         self.len = len(dataset)
-        # np.shuffle(dataset)
-        # dataset = dataset[:(int)(len(dataset) / 10)]
+      
         super().__init__(
             dataset, 
             batch_size=batch_size,
             shuffle=training,
             collate_fn=collate, 
             )
-       # self.vocab = vocab
-
-if __name__=='__main__':
-    """inputs = []
-    with open('./output/qqa_test.pt', 'rb') as f:
-            for _ in range(3):
-                x = pickle.load(f)
-                inputs.append(x)
-                print(type(x), x.shape)"""
+  
+   
